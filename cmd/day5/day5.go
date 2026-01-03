c5351a4812d64b79357eb6f0dfefe8d5289b4fe6
Caposto <christiand.apostol@outlook.com>
2026-01-03 18:12:05 -0500

    completed day 5 part 2

--- cmd/day5/day5.go
+++ cmd/day5/day5.go
@@ -5,13 +5,14 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-// Iterate through input. Add all fresh id ranges to an array
-// Add all valid IDs into a slice
-// Iterate through slice and see if they are present in the map
+// Add all fresh id ranges to an array
+// Add all valid IDs into an array
+// Iterate through slice and see if they are present in the range
 // T: O(n), S: O(n)
 
 type Range struct {
@@ -56,6 +57,7 @@ func readInput(filepath string) ([]Range, []int) {
 	return fresh, validIds
 }
 
+// Part 1
 func countFresh(fresh []Range, valid []int) int {
 	result := 0
 
@@ -71,10 +73,70 @@ func countFresh(fresh []Range, valid []int) int {
 	return result
 }
 
+// Part 2
+// Consolidate overlapping ranges into a lsit of new ranges and return the difference
+func considerFresh(fresh []Range) (int, []Range) {
+	// Sort fresh ranges first by start value to ensure ranges are properly consolidated
+	sort.Slice(fresh, func(i, j int) bool {
+		return fresh[i].start < fresh[j].start
+	})
+
+	newRanges := []Range{fresh[0]} // contains consolidated ranges
+
+	for i := 1; i < len(fresh); i++ {
+		addNewRange := true
+		start := fresh[i].start 
+		end := fresh[i].end
+
+		// compare each existing range to new ranges to see if it can be consolidated
+		for j := 0; j < len(newRanges); j++ {
+			nr := newRanges[j]
+			// In existing range so break
+			if inRange(start, nr.start, nr.end) && inRange(end, nr.start, nr.end) {
+				addNewRange = false
+				break
+			}
+			// Start is in an existing range, but end is not. Expand the end
+			if inRange(start, nr.start, nr.end) && !inRange(end, nr.start, nr.end) {
+				addNewRange = false
+				newRanges[j].end = end
+				break
+			}
+			// Start is not in an existing range, but end is. Expand the start
+			if !inRange(start, nr.start, nr.end) && inRange(end, nr.start, nr.end) {
+				addNewRange = false
+				newRanges[j].start = start
+				break
+			}
+		}
+		// Range did not overlap with any existing range
+		if addNewRange {
+			newRanges = append(newRanges, Range{start, end})
+		}
+	}
+
+	// sum the consolidated ranges up
+	result := 0
+	for _, r := range newRanges {
+		result += r.end - r.start + 1 // ranges are inclusive so add a 1 to account for the ends
+	}
+
+	return result, newRanges
+}
+
+// helper function returns if a value is within start and end
+func inRange(val, start, end int) bool {
+	return val >= start && val <= end
+}
+
 func main() {
 	freshIds, validIds := readInput("./cmd/day5/input.txt")
 	// fmt.Println("Fresh: ", freshIds)
 	// fmt.Println("Valid: ", validIds)
 
-	fmt.Println("Fresh Count: ", countFresh(freshIds, validIds))
+	fmt.Println("Fresh Count: ", countFresh(freshIds, validIds)) // Part 1 Answer: 563
+
+	validFresh, _ := considerFresh(freshIds)
+	// fmt.Println("Valid Fresh: ", newRanges)
+	fmt.Println("Valid Fresh:", validFresh) // Part 2 Answer: 338693411431456
 }
