61278f9ebfe17f26eef93afc753b847b2efa87f2
Caposto <christiand.apostol@outlook.com>
2026-01-14 01:25:45 -0500

    gpt solution for day 10 part 1

--- cmd/day10/day10.go
+++ cmd/day10/day10.go
@@ -71,7 +71,84 @@ func readInput(filepath string) []Machine {
 	return machines
 }
 
+// Helper function for converting EndState to a bitstring where "." = 0 and "#" = 1
+// .##. = 0110
+func diagramToMask(diagram []string) uint64 {
+	var mask uint64
+	for i, c := range diagram {
+		if c == "#" {
+			mask |= 1 << i // x |= y is shorthand for x = x | y
+		}
+	}
+	return mask
+}
+
+// Helper function for converting a button to a bitstring, where each number in the button correlates to a "1"
+// (1, 3) on a 4 character endstate would equal "0101"
+func buttonToMask(button []int) uint64 {
+	var mask uint64
+	for _, idx := range button {
+		mask |= 1 << idx
+	}
+	return mask
+}
+
+// Parent function for calling DFS
+func minPresses(target uint64, buttons []uint64) int {
+	n := len(buttons)
+
+	// try subsets of size k = 0..n
+	for k := 0; k <= n; k++ {
+		if dfs(0, 0, 0, k, target, buttons) {
+			return k
+		}
+	}
+	return -1 // should never happen if solvable
+}
+
+// Is BFS better
+// TODO: Implement in BFS
+func dfs(
+	start int,
+	pressed int,
+	state uint64,
+	limit int,
+	target uint64,
+	buttons []uint64,
+) bool {
+	if pressed == limit {
+		return state == target
+	}
+
+	for i := start; i < len(buttons); i++ {
+		if dfs(i+1, pressed+1, state^buttons[i], limit, target, buttons) {
+			return true
+		}
+	}
+	return false
+}
+
+
+// Hints: https://www.reddit.com/r/adventofcode/comments/1pl60n7/2025_day10_part_1_what_is_the_intuition_here_folks/
+func MinimumMatches(machines []Machine) int {
+	total := 0
+
+	for _, m := range machines {
+		target := diagramToMask(m.EndState)
+
+		buttonMasks := make([]uint64, len(m.Buttons))
+		for i, b := range m.Buttons {
+			buttonMasks[i] = buttonToMask(b)
+		}
+
+		total += minPresses(target, buttonMasks)
+	}
+
+	return total
+}
+
+
 func main() {
-	machines := readInput("./cmd/day10/input2.txt")
-	fmt.Println(machines)
+	machines := readInput("./cmd/day10/input.txt")
+	fmt.Println(MinimumMatches(machines)) // Part 1 Answer: 535
 }
